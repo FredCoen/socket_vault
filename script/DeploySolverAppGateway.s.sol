5a57f3057de66f0f077e529299a4a37f5ff879d9
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2025-04-16 15:59:04 +0530

    fix: scripts

--- script/DeploySolverAppGateway.s.sol
+++ script/DeploySolverAppGateway.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
+pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
@@ -8,22 +8,40 @@ import {ETH_ADDRESS} from "socket-protocol/protocol/utils/common/Constants.sol";
 
 import {SolverAppGateway} from "../src/SolverAppGateway.sol";
 
+import {SpokePoolWrapper} from "../src/SpokePoolWrapper.sol";
+import {WETHVault} from "../src/Vault.sol";
+
+// source .env && forge script script/DeploySolverAppGateway.s.sol --broadcast --skip-simulation --legacy --gas-price 0
 contract SolverAppGatewayDeploy is Script {
     function run() external {
         address addressResolver = vm.envAddress("ADDRESS_RESOLVER");
-        address spokePoolArbitrum = vm.envAddress("SPOKE_POOL_421614");
-        address spokePoolBase = vm.envAddress("SPOKE_POOL_84532");
+        address spokePoolArbitrum = address(1);
+        address spokePoolBase = address(2);
 
         string memory rpc = vm.envString("EVMX_RPC");
         vm.createSelectFork(rpc);
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Fees memory fees = Fees({feePoolChain: 421614, feePoolToken: ETH_ADDRESS, amount: 0.01 ether});
+        Fees memory fees = Fees({
+            feePoolChain: 421614,
+            feePoolToken: ETH_ADDRESS,
+            amount: 0.001 ether
+        });
 
-        SolverAppGateway appGateway = new SolverAppGateway(addressResolver, fees, spokePoolArbitrum, spokePoolBase);
+        SolverAppGateway appGateway = new SolverAppGateway(
+            addressResolver,
+            fees,
+            spokePoolArbitrum,
+            spokePoolBase,
+            abi.encodePacked(type(SpokePoolWrapper).creationCode),
+            abi.encodePacked(type(WETHVault).creationCode)
+        );
 
         console.log("SolverAppGateway contract:", address(appGateway));
-        console.log("See SolverAppGateway on EVMx: https://evmx.cloud.blockscout.com/address/%s", address(appGateway));
+        console.log(
+            "See SolverAppGateway on EVMx: https://evmx.cloud.blockscout.com/address/%s",
+            address(appGateway)
+        );
     }
 }
--- script/PayFeesInArbitrumEth.s.sol
+++ script/PayFeesInArbitrumEth.s.sol
@@ -1,27 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
+pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {FeesPlug} from "socket-protocol/protocol/payload-delivery/FeesPlug.sol";
 import {ETH_ADDRESS} from "socket-protocol/protocol/utils/common/Constants.sol";
 
+// source .env && forge script script/PayFeesInArbitrumEth.s.sol  --broadcast --skip-simulation --legacy --gas-price 0
 contract DepositFees is Script {
     function run() external {
-        vm.createSelectFork(vm.envString("RPC_421614"));
+        vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC"));
 
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
-        FeesPlug feesPlug = FeesPlug(payable(vm.envAddress("ARBITRUM_FEES_PLUG")));
+        FeesPlug feesPlug = FeesPlug(
+            payable(vm.envAddress("ARBITRUM_FEES_PLUG"))
+        );
         address appGateway = vm.envAddress("APP_GATEWAY");
 
         address sender = vm.addr(privateKey);
         uint256 balance = sender.balance;
         console.log("Using address %s with %s balance in wei", sender, balance);
 
-        uint256 feesAmount = 0.05 ether;
-        feesPlug.deposit{value: feesAmount}(ETH_ADDRESS, appGateway, feesAmount);
-        console.log("Added fee balance for AppGateway %s", feesAmount, appGateway);
+        uint256 feesAmount = 0.005 ether;
+        feesPlug.deposit{value: feesAmount}(
+            ETH_ADDRESS,
+            appGateway,
+            feesAmount
+        );
+        console.log(
+            "Added fee balance for AppGateway %s",
+            feesAmount,
+            appGateway
+        );
         vm.stopBroadcast();
     }
 }
